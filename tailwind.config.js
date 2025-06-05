6f52d8044e402aac41eb7be9307baabff9cb832e
Mohamed Tawfeeq <mtawfeeqmrf@gmail.com>
2025-06-05 20:41:13 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './sections/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
