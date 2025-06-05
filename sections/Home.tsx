da6018864cd7919aef15953151273f1e41a12aaa
Mohamed Tawfeeq <mtawfeeqmrf@gmail.com>
2025-06-05 20:43:43 +0200

    Create Home.tsx

---
+++ sections/Home.tsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+export default function HomeSection() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
